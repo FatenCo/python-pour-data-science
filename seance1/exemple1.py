b386d2982d1e853247b7f88db955e8d01804bb60
faten mekni <fatenmekni092@gmail.com>
2024-02-01 02:51:02 -1100

    Initial commit

---
+++ seance1/exemple1.py
@@ -0,0 +1,6 @@
+nom="faten" #string
+age=30 #int
+salaire=1200.50 #float
+situation = True #bolean
+print("employe",nom, "son age : ", age, "son salire :", salaire, "son situation est : ", situation)
+# git status : donner l'etat du repot
